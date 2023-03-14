366afcce3e711d3693e6a647bf6e7ecb51f15e26
Vitor Praxedes Calegari <vitorprca@gmail.com>
2023-03-14 18:24:46 -0300

    Adicionando o arquivo Engine.py

---
+++ Engine.py
@@ -0,0 +1,40 @@
+class Engine():
+    
+    def __init__(self, letra: str()):
+        self.__acertou = bool()
+        self.__repetida = bool()
+        self.__letra = letra
+        
+    '''
+    Verifica se a letra já foi utilizada, e se a letra está contida na palavra ou não.
+    
+    Obs: Se a letra estiver repetida a função de verificação não verifica se letra
+         está ou não na palavra atual, ela apenas altera o atributo "self.__repetida"
+         para "True", e por padrão se apenas classificamos uma variável como boolena
+         em python(como no __init__ dessa classe) ela tem valor base "False", logo
+         deve-se ter cuidado para checar se a letra está repetida ou não antes
+         de utilizar o getter de acerto para qualquer propósito.
+    
+    '''
+    def verifica(self, letras_utilizadas: list(), palavra_atual: str()):
+        if self.__letra not in letras_utilizadas:
+            if self.__letra in palavra_atual:
+                self.__acertou = True
+                self.__repetida = False
+            else:
+                self.__acertou = False
+                self.__repetida = False
+        else:
+            self.__repetida = True
+        
+        return
+        
+    def get_letra(self): #Retorna a letra já testada
+        return self.__letra
+    
+    def get_acerto(self): #Retorna se a letra está contida na palavra atual
+        return self.__acertou
+    
+    def get_repetida(self): #Retorna se a letra já foi proposta pelo usuário anteriormente
+        return self.__repetida
+    

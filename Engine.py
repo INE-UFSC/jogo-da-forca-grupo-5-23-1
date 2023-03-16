007584337e2bc16ca850948eb91394d14f284e88
Viccenzo Escarrone <viccenzoescarrone@gmail.com>
2023-03-16 09:32:31 -0300

    Adicionada lista de palavras e random para sorteio de um das palavras

--- Engine.py
+++ Engine.py
@@ -1,4 +1,12 @@
+import random
+
 class Engine():
+    def palavra(self):
+        self.palavras = ['amigo', 'arvore', 'banco', 'bebida', 'bicicleta', 'casa', 'comida', 'dia', 'dinheiro', 'escola', 'familia', 'feira', 'festa', 'fogo', 'frio', 'gato', 'hospital', 'igreja', 'jardim', 'livro', 'lua', 'maquina', 'mar', 'mundo', 'noite', 'ovo', 'pao', 'pessoa', 'piscina', 'porta', 'prato', 'sapato', 'sol', 'terra', 'trabalho', 'vaca', 'voo', 'agua', 'cidade', 'coracao', 'elefante', 'futebol', 'garrafa', 'grama', 'hotel', 'iglu', 'ilha', 'imagem', 'janela', 'jornal', 'limao', 'limpeza', 'loja', 'mala', 'mao', 'mesa', 'montanha', 'navio', 'neve', 'nota', 'novo', 'olho', 'onda', 'orelha', 'ouro', 'papel', 'pato', 'peixe', 'piano', 'pincel', 'ponte', 'presente', 'radio', 'rio', 'robo', 'roupa', 'sala', 'sapo', 'telefone', 'tenis', 'tempo', 'terrao', 'texto', 'tigre', 'trem', 'uva', 'vento', 'verao', 'viagem', 'vida', 'vidro', 'vinho', 'yoga', 'zebra', 'zero'] #lista de palavras sem acento e sem 'ç'
+
+    def palavra_aleatoria(self):
+        return random.choice(self.palavras) #sorteia aleatoriamente uma das palavras da lista 
+
     
     def __init__(self, letra: str()):
         self.__acertou = bool()
